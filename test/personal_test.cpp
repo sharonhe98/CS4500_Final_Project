b52141e6a4ea4db8309d199bba3d8e703e917c76
Sharon He <sharonhe98@login-students.ccs.neu.edu>
2020-03-12 13:16:44 -0400

    [MODIFY] test 2

--- test/personal_test.cpp
+++ test/personal_test.cpp
@@ -22,18 +22,45 @@ void testArray() {
 
 void test() {
   Schema s("II");
-  printf("schema cols %i rows %i\n", s.width(), s.length());
+ // printf("schema cols %i rows %i\n", s.width(), s.length());
   printf("SCHEMA IS BUILT!!\n");
   DataFrame df(s);
   printf("DATAFRAME IS BUILT!\n");
   Row  r(df.get_schema());
   printf("ROW IS BUILLTTTTT!\n");
-  for(size_t i = 0; i <  100 * 100; i++) {
-    printf("working?!!\n");
+  for(size_t i = 0; i <  1000 * 1000; i++) {
+  //  printf("working?!!\n");
     r.set(0,(int)i);
-    printf("INT SET 1!!\n");
+    //printf("INT SET 1!!\n");
     r.set(1,(int)i+1);
-    printf("INT SET 2!!\n");
+    //printf("INT SET 2!!\n");
+    df.add_row(r);
+    printf("ADD ROW PASSED!\n");
+    printf("row num %i\n", df.nrows());
+    printf("col num %i\n", df.ncols());
+  }
+  
+  exit(0);
+}
+
+
+void test2() {
+  Schema s("IISFB");
+ // printf("schema cols %i rows %i\n", s.width(), s.length());
+ // printf("SCHEMA IS BUILT!!\n");
+  DataFrame df(s);
+  printf("DATAFRAME IS BUILT!\n");
+  Row  r(df.get_schema());
+  printf("ROW IS BUILLTTTTT!\n");
+  for(size_t i = 0; i <  100 * 100; i++) {
+ //   printf("working?!!\n");
+    r.set(0,(int)i);
+   // printf("INT SET 1!!\n");
+    r.set(1,(int)i+1);
+    r.set(2, new String("hello world"));
+    r.set(3, (float)2.0);
+    r.set(4, (bool)1);
+    //printf("INT SET 2!!\n");
     df.add_row(r);
     printf("ADD ROW PASSED!\n");
     printf("row num %i\n", df.nrows());
@@ -46,5 +73,6 @@ void test() {
 
 int main(int argc, char **argv) {
   // testArray();
-	test();
+	//test();
+	test2();
 }
