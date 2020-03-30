fd2710e3abaab965b5a5d653d7b25fa093c2ccb6
Sharon He <sharonhe98@login-students.ccs.neu.edu>
2020-03-30 18:56:05 -0400

    [ADD] Add tests for from etc and kvstore tests

---
+++ test/dataframe_from_test.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cassert>
+#include "../src/kvstore.h"
+
+void test_from_array() {
+	size_t SZ = 100;
+	double* fa = new double[SZ];
+	for (size_t i = 0; i < SZ; i++) {
+		fa[i] = 5.0;
+	}
+	Key* k = new Key("fa", 0);
+	KVStore* kv = new KVStore(0);
+	DataFrame* df = DataFrame::fromArray(k, kv, SZ, fa);
+	Serializer ser;
+	Value* v = df->serialize(ser);
+	assert(df->get(0, 30) == 5.0);
+	assert(df->nrows() == 100);
+	assert(kv->get(k)->equals(v));
+	printf("test fromArray success!\n");
+}
+
+void test_from_scalar() {
+	double sca = 500.0;
+	Key* k = new Key("f", 0);
+	KVStore* kv = new KVStore(0);
+	DataFrame* df = DataFrame::fromScalar(k, kv, sca);
+	Serializer ser;
+	Value* v = df->serialize(ser);
+	assert(df->get(0, 0) == 500.0);
+	assert(kv->get(k)->equals(v));
+	printf("test fromScalar success!\n");
+}
+
+int main() {
+	test_from_array();
+	test_from_scalar();
+	return 0;
+}
---
+++ test/kvstore_test.cpp
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <cassert>
+#include <thread>
+#include "../src/kvstore.h"
+
+using namespace std;
+
+void testGet() {
+	Schema s("II");
+	DataFrame* df = new DataFrame(s);
+	KVStore* kv = new KVStore(0);
+	Key* key = new Key("df_key", 0);
+	assert(!kv->get(key));
+	Serializer ser;
+	Value* v = df->serialize(ser);
+	kv->put(key, v);
+	assert(kv->get(key)->equals(v));
+	printf("kv store get success!\n");
+}
+
+void testWaitAndGet() {
+	Schema s("II");
+	DataFrame* df = new DataFrame(s);
+	Key* key = new Key("df_key", 0);
+	Serializer ser;
+	Value* v = df->serialize(ser);
+
+	KVStore** all_nodes = new KVStore*[3];
+	for (size_t i = 0; i < 3; i++) {
+		all_nodes[i] = new KVStore(i);
+	}
+	// we don't yet have a network layer, but this would be our expected result
+//	assert(all_nodes[1]->waitAndGet(key)->equals(df));
+//	all_nodes[0]->put(key, v);
+}
+
+int main() {
+	testGet();
+	return 0;
+}
